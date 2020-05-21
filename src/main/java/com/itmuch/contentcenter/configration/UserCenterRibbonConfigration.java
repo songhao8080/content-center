6fa261536c99d56edbe80687b21ea728e3873671
宋军辉 <songjunhui@zycfcinc.com>
2020-05-21 16:50:08 +0800

    细粒度配置 ribbon 配置方式

--- src/main/java/com/itmuch/contentcenter/configration/UserCenterRibbonConfigration.java
+++ src/main/java/com/itmuch/contentcenter/configration/UserCenterRibbonConfigration.java
@@ -1,4 +1,15 @@
 package com.itmuch.contentcenter.configration;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+import ribbonconfigration.RibbonConfigration;
+
+/**
+ * java 代码配置 Ribbon
+ */
+//@Configuration
+//@RibbonClient(name = "user-center",configuration = RibbonConfigration.class)
 public class UserCenterRibbonConfigration {
+
+
 }
--- src/main/java/ribbonconfigration/RibbonConfigration.java
+++ src/main/java/ribbonconfigration/RibbonConfigration.java
@@ -1,4 +1,19 @@
 package ribbonconfigration;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 随机轮询
+ */
+
+@Configuration
 public class RibbonConfigration {
+    @Bean
+    public IRule ribbonRule(){
+        return new RandomRule();
+    }
+
 }
